3f55ecb43e4f84070db0224cc7edea10219fce00
MANISH KUMAR <155057422+Manishk1245@users.noreply.github.com>
2025-04-06 19:38:55 +0000

    datatypes

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,17 @@
+let name  = "manish";
+let age = 3;
+let isLogin = false
+let state;
+
+
+//number 2 t power 53
+//BigInt 
+//string = " "
+//boolean= false/true
+//null = standalone value
+//undefined = 
+//symbol = unique
+
+
+// object
+console.log(typeof "manish");
