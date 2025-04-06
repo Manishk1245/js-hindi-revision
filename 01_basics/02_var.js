aa6a369f81911657bf5efe84e74de18b45272051
MANISH KUMAR <155057422+Manishk1245@users.noreply.github.com>
2025-04-06 19:39:51 +0000

    variables

---
+++ 01_basics/02_var.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "mainshkumar636@gmail.com";
+var accountPassword = "12345"
+accountCity= "Jaipur"
+let accountState;
+
+
+// accountId = 145225
+accountEmail="vhgv@gmail.com";
+accountPassword = 12352;
+accountCity="bengaluru";
+
+console.log(accountId)
+
+  console.table([accountId, accountEmail,accountPassword, accountCity,accountState])
